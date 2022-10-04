7ea9796d1e6079a5a41fc293a2624b6a02d386ee
palashichevaa@mail.ru <palashichevaa@mail.ru>
2022-10-04 13:39:52 +1000

    код1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Drawing;
+using System.IO;
 
 namespace ISRPO_Palashicheva_PR5
 {
@@ -23,6 +26,28 @@ namespace ISRPO_Palashicheva_PR5
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void cat1_Click(object sender, RoutedEventArgs e)
+        {
+            img1.Source = new BitmapImage(new Uri("Resources/cat_space.png", UriKind.Relative));
+        }
+
+        private void cat2_Click(object sender, RoutedEventArgs e)
+        {
+            img1.Source = new BitmapImage(new Uri("Resources/kotcosmos.jpg", UriKind.Relative));
+        }
+
+        private void OpenD_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)|*.bmp;*.jpg;*.gif; *.tif; *.png; *.ico; *.emf; *.wmf";
+            if (fileDialog.ShowDialog() == true)
+            {
+                //fileDialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)|*.bmp;*.jpg;*.gif; *.tif; *.png; *.ico; *.emf; *.wmf";
+                string fileName = fileDialog.FileName;
+                img1.Source = new BitmapImage(new Uri(fileName));
+            }
         }
     }
 }
